97647099cd64273723aed35f7943a04527a0b942
Andalv323 <59402646+Andalv323@users.noreply.github.com>
2023-04-12 18:56:25 -0700

    Update to Ruth's Version

---
+++ __MACOSX/Salon/Salon/Models/Service.swift
@@ -0,0 +1,50 @@
+//
+//  Service.swift
+//  SalonAppProject
+//
+//  Created by user230516 on 3/29/23.
+//
+
+import Foundation
+import ParseSwift
+enum status {
+    case booked
+    case cancelled
+    case completed
+}
+enum gender {
+    case male
+    case female
+    case other
+}
+enum cancelled_by {
+    case customer
+    case business
+    case null
+}
+
+struct Service: ParseObject {
+    var originalData: Data?
+    var objectId: String?
+    var createdAt: Date?
+    var updatedAt: Date?
+    var ACL: ParseSwift.ParseACL?
+    var name: String?
+    var price: Decimal?
+    var duration: Int?
+    var business_id: Int? //(foreign key referencing Business)
+    
+    var Appointment_id: Int?
+    
+    var customer_id: Int?
+    
+    var service_id: Int? //(foreign key referencing Service)
+    
+    var date_time: Date?
+    
+    var status : String?
+    
+    var gender: String?
+    
+    var cancelled_by: String?
+}
